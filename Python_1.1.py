d78f780035207b0428b0e0bdda7220c5a0da95a7
Giulia <giuliabelicuas2@gmail.com>
2024-10-28 22:58:54 -0300

    Adiciona script para a questão 1

---
+++ Python_1.1.py
@@ -0,0 +1,18 @@
+
+#dicionario 
+
+#!/usr/bin/python
+#Criando um dicionario com minhas coisas favoritas
+
+fav_dict = {
+     "livro": "Harry Potter e o cálice de fogo",
+     "som": "O grilo",
+     "arvore":"amoreira"
+}
+
+for chave, valor in fav_dict.items():
+    print(f"{chave}: {valor}")
+
+
+
+
