6aac4a9226bf1328fe3741d00b67f000fc07d1b2
Giulia <giuliabelicuas2@gmail.com>
2024-10-28 23:15:48 -0300

    Adiciona script para print o seu livro favorito, mas use uma variável na chave

---
+++ Python05_1.3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#Criando um dicionario com minhas coisas favoritas
+
+fav_dict = {
+     "livro": "Harry Potter e o cálice de fogo",
+     "som": "O grilo",
+     "arvore":"amoreira"
+}
+#variável para achar o livro favorito
+
+fav_thing = 'livro'
+print (fav_dict[fav_thing])
