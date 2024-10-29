bf4479536ec7c1c203a928c5d07d1b3dfc29d7af
Giulia <giuliabelicuas2@gmail.com>
2024-10-28 23:25:57 -0300

    Adiciona script adicionando o organismo favorito ao dicionário, onde o organismo é o novo valor da chave fav_thing

---
+++ Python05_1.5.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#Criando um dicionario com minhas coisas favoritas
+
+fav_dict = {
+     "livro": "Harry Potter e o cálice de fogo",
+     "som": "O grilo",
+     "arvore":"amoreira"
+}
+
+#adicionar um novo organismo em fav_dict
+
+fav_dict['organismo'] = 'Cachorro'
+fav_thing = 'organismo'
+print (fav_dict[fav_thing])
+
+
