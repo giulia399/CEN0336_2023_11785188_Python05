2fa196659343349125bb02003cfffd1373fcaf41
Giulia <giuliabelicuas2@gmail.com>
2024-10-28 23:20:36 -0300

    Adiciona script para print da arvore favorita

---
+++ Python05_1.4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#Criando um dicionario com minhas coisas favoritas
+
+fav_dict = {
+     "livro": "Harry Potter e o cálice de fogo",
+     "som": "O grilo",
+     "arvore":"amoreira"
+}
+
+#imprimir o valor associado a arvore
+
+print (fav_dict['arvore'])
