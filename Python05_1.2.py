e3f47cb71bbb42cb5e717cbfe67c80d5d08b0d4e
Giulia <giuliabelicuas2@gmail.com>
2024-10-28 23:08:12 -0300

    Renomeia diretório de Python_1.1.py para Python05_1.1.py

---
+++ Python05_1.2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#Criando um dicionario com minhas coisas favoritas
+
+fav_dict = {
+     "livro": "Harry Potter e o cálice de fogo",
+     "som": "O grilo",
+     "arvore":"amoreira"
+}
+
+
+#imprimir o valor associado a livro
+
+print (fav_dict['livro'])
