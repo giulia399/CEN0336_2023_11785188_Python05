45dd1ba539a2736f5c1051d1d4c8d42a2d5dca5a
Giulia <giuliabelicuas2@gmail.com>
2024-10-31 08:08:10 -0300

    Renomeando arquivos Python

---
+++ Python05_2.1.py
@@ -0,0 +1,10 @@
+#Crie um conjunto usando as duas sintaxes diferentes para criar um conjunto, 
+#myset = set() e myset2 = {}. Qual é a diferença? Importa como você cria o conjunto?
+#!/usr/bin/python
+
+# Criação dos conjuntos
+mySet = set('ATGTGGG')  # Conjunto criado a partir de uma string
+mySet2 = {'ATGCCT'}     # Conjunto criado usando a sintaxe literal
+
+print("mySet:", mySet)
+print("mySet2:", mySet2)
