11a92eca4e0a6930144b70faa1b5bd80bafc4c45
Giulia <giuliabelicuas2@gmail.com>
2024-10-31 07:31:59 -0300

    fav_thing da linha de comando e um novo valor para essa chave

---
+++ Python05_1.8.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# Criando um dicionário com minhas coisas favoritas
+
+fav_dict = {
+    "livro": "Harry Potter e o Cálice de Fogo",
+    "som": "O Grilo",
+    "arvore": "Amoreira"
+}
+
+# Adicionar um novo organismo em fav_dict
+fav_dict['organismo'] = 'Cachorro'
+fav_thing = 'organismo'
+print(f"O organismo favorito é: {fav_dict[fav_thing]}")
+
+# Alterando o valor do organismo
+if fav_thing in fav_dict:  # Verificar se a chave existe
+    fav_dict[fav_thing] = 'Pinguim'  # Trocar o valor para 'Pinguim'
+
+# Mostra o novo valor do organismo
+print(f"O novo organismo favorito é: {fav_dict[fav_thing]}")
